1ab482f993ca167b116e19e1aeb16b8f4f3f7fd3
Anna Margery <anna.maria.margery@gmail.com>
2026-07-14 16:56:33 +0200

    introduced Address Entity, organized project

---
+++ src/main/java/com/app/ecom/model/UserRole.java
@@ -0,0 +1,8 @@
+package com.app.ecom.model;
+
+public enum UserRole {
+
+    ADMIN,
+    CUSTOMER,
+
+}
--- src/main/java/com/app/ecom/service/UserService.java
+++ src/main/java/com/app/ecom/service/UserService.java
@@ -1,39 +1,38 @@
-package com.app.ecom;
+package com.app.ecom.service;
 
+import com.app.ecom.model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.app.ecom.repository.UserRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    private List<User> users = new ArrayList<>();
-    private Long nextId = 1L;
+    private final UserRepository userRepository;
+
 
     public List<User> fetchAllUsers() {
-        return users;
+        return userRepository.findAll();
     }
 
     public void addUser(User user) {
-        user.setId(nextId++);
-        users.add(user);
+        userRepository.save(user);
     }
 
     public Optional<User> getUserById(Long id) {
-        return users.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+        return userRepository.findById(id);
     }
 
     public boolean updateUser(Long id, User updatedUser) {
-        return users.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setFirstName(updatedUser.getFirstName());
                     existingUser.setLastName(updatedUser.getLastName());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
     }
